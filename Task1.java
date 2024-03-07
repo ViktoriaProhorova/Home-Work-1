c2450f86d2a7ad44114512edd33ac4bec92696df
ViktoriaProhorova <viktoriyaprokhorova7@gmail.com>
2024-03-07 13:37:59 +0100

    Information about myself

---
+++ Task1.java
@@ -0,0 +1,9 @@
+public class Task1
+{
+    public static void main(String[] args) {
+            String name = "Viktoria";
+            String lastName = "Prohorova";
+            String profession = "QA Engineer";
+            System.out.println(name + "\n" + lastName + "\n" + profession);
+        }
+}
