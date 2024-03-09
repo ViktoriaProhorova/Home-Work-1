3ae27e4cf28ea88850c6ebbb2da7c3fe65aca968
ViktoriaProhorova <viktoriyaprokhorova7@gmail.com>
2024-03-09 17:00:51 +0100

    Update for main with necessary information.

---
+++ Task2.java
@@ -0,0 +1,24 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte myByte = 7;
+        short myShort = -3198;
+        int myInt = 12345;
+        long myLong = -214748364;
+        float myFloat = 19.99F;
+        double myDouble = 30.4;
+        boolean myBoolean = true;
+        char myChar ='&';
+        String myString = "John";
+
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+        System.out.println(myString);
+
+    }
+}
